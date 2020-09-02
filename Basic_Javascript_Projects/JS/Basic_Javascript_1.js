44677b0d06e126b0aea40ff7acf4e7416103c4b8
KingKongBingBong <63321771+KingKongBingBong@users.noreply.github.com>
2020-09-02 10:57:39 -0700

    Create Basic JS Project

---
+++ Basic_Javascript_Projects/JS/Basic_Javascript_1.js
@@ -0,0 +1,14 @@
+
+var dog = 'Sandy', cat = "Bella", penguin = "Hubie"; //assigning values to variables
+
+dog = dog.fontcolor("green"); //changing dog text to green
+cat = cat.fontcolor("purple"); //changing cat text to purple
+penguin = penguin.fontcolor("red"); //changing penguin text to red
+
+var message = "Here's an alert!" //declaring message variable and assigning string value
+
+window.alert(message); //pop up window displaying alert message
+
+document.write(dog + " " + cat + " " + penguin); //concatenating the variables
+
+var answer = 6 + 6; //writing an expression
