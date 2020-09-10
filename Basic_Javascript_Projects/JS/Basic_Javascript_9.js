667fb8ffaccc63a0a1ee9c7097b173c49116a996
KingKongBingBong <63321771+KingKongBingBong@users.noreply.github.com>
2020-09-09 22:24:16 -0700

    add basic javascript project 9
    countdown timer and slideshow

---
+++ Basic_Javascript_Projects/JS/Basic_Javascript_9.js
@@ -0,0 +1,46 @@
+function countdown() { //countdown timer example. enter time -> countdown to 0 -> alert time is up
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+    if (seconds == -1) {
+        alert("Time's up!");
+    }
+    }
+    tick();
+}
+
+// ******Slideshow section******
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// next/prev controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
